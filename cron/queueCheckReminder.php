4047ab2442a72cadff59ab28b0957fb932bec86e
TheL0ki <theloki1712@gmail.com>
2025-02-12 16:52:57 +0100

    bugfixing Cronjob

---
+++ cron/queueCheckReminder.php
@@ -0,0 +1,44 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+chdir(dirname(__DIR__));
+require_once 'vendor/autoload.php';
+
+require 'config/setup.php';
+require 'config/func.php';
+
+$date = new DateTime();
+$logdate = $date->format('Y-m-d H:i:s');
+$date->modify('next Saturday');
+
+$day = $date->format('d');
+$month = $date->format('m');
+$year = $date->format('Y');
+
+$select = 'SELECT user FROM schedule WHERE day = ? AND month = ? and year = ?';
+$stmt = $mysqli->prepare($select);
+$stmt->bind_param('sss', $day, $month, $year);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$url = "https://hooks.slack.com/triggers/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";
+
+
+while($row = $result->fetch_array()) {
+    $user = getUser($row[0]);
+}
+
+$ch = curl_init($url);
+$payload = json_encode([
+    'user' => $user['slackID']
+]);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo '[' . $logdate . '] Cron executed, User "' . $user['firstname'] . ' ' . $user['lastname'] . '" with SlackID "' . $user['slackID'] . '" has been notified. ' . "\r\n";
